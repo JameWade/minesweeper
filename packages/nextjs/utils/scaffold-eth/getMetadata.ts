db7a728487ce19a32bb96067830f3f2434545dee
jamewade <qq1729128876@163.com>
2025-03-18 23:13:49 +0800

    add thumbnail

--- packages/nextjs/utils/scaffold-eth/getMetadata.ts
+++ packages/nextjs/utils/scaffold-eth/getMetadata.ts
@@ -8,7 +8,7 @@ const titleTemplate = "%s | Scaffold-ETH 2";
 export const getMetadata = ({
   title,
   description,
-  imageRelativePath = "/mine.svg",
+  imageRelativePath = "/thumbnail.png",
 }: {
   title: string;
   description: string;
