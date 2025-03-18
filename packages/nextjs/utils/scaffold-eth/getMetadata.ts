de8f48f6dfa0a44da70f84533e2edfc25eeccce4
jamewade <qq1729128876@163.com>
2025-03-18 23:16:35 +0800

    change thumbnail

--- packages/nextjs/utils/scaffold-eth/getMetadata.ts
+++ packages/nextjs/utils/scaffold-eth/getMetadata.ts
@@ -8,7 +8,7 @@ const titleTemplate = "%s | Scaffold-ETH 2";
 export const getMetadata = ({
   title,
   description,
-  imageRelativePath = "/thumbnail.png",
+  imageRelativePath = "/thumbnail.jpg",
 }: {
   title: string;
   description: string;
